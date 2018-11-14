b87c5d87075874fbf29ecf482653bda5a8bc93a9
Garrett Tichy <gt-home@Garretts-iMac-2.local>
2018-11-14 17:05:19 -0500

    jamming structure - step 31

---
+++ jammming/jammming/src/Components/App/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Playlist from '../Playlist/Playlist.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+import SearchResults from '../SearchResults/SearchResults.js';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{
+        name: 'Cher',
+        artist: 'cher',
+        album: 'strong enough',
+        id: '323'
+      }]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+          <SearchResults searchResults={this.state.searchResults}/>
+          <Playlist />
+          </div>
+        </div>
+      </div>
+        
+    );
+  }
+}
+
+export default App;
