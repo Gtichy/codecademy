064f36974709ca27d5ffb7b9831583b3aa3f0031
Garrett Tichy <garrett@smpl.io>
2018-11-14 09:01:54 -0500

    Completed Ravenous project & setup for Jammming

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term,location,sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
   render() {
     return (
-      <div class="App">
+      <div className="App">
         <h1>ravenous</h1>
-
-        <SearchBar/>
-        <BusinessList/>
-
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
