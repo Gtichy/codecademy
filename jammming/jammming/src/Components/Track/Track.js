c85a07bf7a40bbe533b7472cb83834f4b28e76ef
Garrett Tichy <gt-home@Garretts-iMac-2.local>
2018-11-15 11:33:40 -0500

    jammming - step 56

--- jammming/jammming/src/Components/Track/Track.js
+++ jammming/jammming/src/Components/Track/Track.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './Track.css';
 
 class Track extends Component {
+    constructor(props){
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+        this.renderAction = this.renderAction.bind(this);
+    }
     
-     /* renderAction(){
-        if(isRemoval){
-            return '-';
+    addTrack(){
+        this.props.onAdd(this.props.track);
+    }
+
+    removeTrack(){
+        this.props.onRemove(this.props.track);
+    }
+
+      renderAction(){
+
+        if(this.props.isRemoval){
+            return <a onClick={this.removeTrack} className="Track-action">-</a>;
         }else{
-            return '+';
+            return <a onClick={this.addTrack} className="Track-action">+</a>;
         }
-    };*/
+    };
 
         render() {
             return (
@@ -18,7 +33,7 @@ class Track extends Component {
                     <h3>{this.props.track.name}</h3>
                         <p>{this.props.track.artist} | {this.props.track.album}</p>
                     </div>
-                    <a className="Track-action"></a>
+                    {this.renderAction()}
                 </div>
             )
         }
--- jammming/jammming/src/Components/TrackList/TrackList.js
+++ jammming/jammming/src/Components/TrackList/TrackList.js
@@ -8,7 +8,7 @@ class TrackList extends Component {
             <div className="TrackList">
             {
             this.props.tracks.map(track => {
-                return <Track key={track.id} track={track}/>
+                return <Track isRemoval={this.props.isRemoval} onAdd={this.props.onAdd} onRemove={this.props.onRemove} key={track.id} track={track}/>
             })
             }
             </div>
