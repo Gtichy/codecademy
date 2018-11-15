85a12940eb34afe651149480143f553f1d6cb11a
Garrett Tichy <gt-home@Garretts-iMac-2.local>
2018-11-15 11:21:57 -0500

    jammming - step 56

--- jammming/jammming/src/Components/App/App.js
+++ jammming/jammming/src/Components/App/App.js
@@ -7,40 +7,75 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+
     this.state = {
       searchResults: [{
-        name: 'Cher',
-        artist: 'cher',
-        album: 'strong enough',
-        id: '323'
+        name: 'Algorithm',
+        artist: 'Muse',
+        album: 'Suspended Reality',
+        id: '5'
+      },{
+        name: 'Drones',
+        artist: 'Muse',
+        album: 'Suspended Reality',
+        id: '6'
       }],
       playlistName: 'Garretts Playlist',
       playlistTracks: [{
-        name: 'Cher',
-        artist: 'cher',
-        album: 'strong enough',
+        name: 'Supersonic',
+        artist: '311',
+        album: 'modest',
         id: '1'
       },{
-        name: 'Cher 2',
+        name: 'Cher',
         artist: 'cher',
         album: 'strong enough',
         id: '2'     
       },{
-        name: 'Cher 3',
-        artist: 'cher',
-        album: 'strong enough',
+        name: 'suicidal',
+        artist: 'Metric',
+        album: 'Paper Airplanes',
         id: '3'
 
-      }] 
+      }],
+      newTrack: {
+          name: 'Garbo singer',
+          artist: 'The Garb',
+          album: 'so strong',
+          id: '4'
+      }
     }
   }
   
   addTrack(track){
-    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+    const { playlistTracks } = this.state;
+    const newTracks = playlistTracks;
+
+   if (playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     }else{
-      
+      newTracks.push(track);
     }
+
+    this.setState({ playlistTracks: newTracks });
+  }
+
+  removeTrack(track){
+    const { playlistTracks } = this.state;
+    const currentTracks = playlistTracks;
+
+   if (playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      const currentTrackIndex = currentTracks.indexOf(track);
+      currentTracks.splice(currentTrackIndex, 1);
+
+    }else{
+      return;
+    }
+
+    this.setState({ playlistTracks: currentTracks });
   }
 
   render() {
@@ -48,10 +83,11 @@ class App extends Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
+
           <SearchBar />
           <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults}/>
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <Playlist onRemove={this.removeTrack} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
