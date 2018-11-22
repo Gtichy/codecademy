06c9fa13258bad3248badd687bf7cc8067e47a8f
Garrett Tichy <garrett@smpl.io>
2018-11-22 12:35:07 -0500

    User playlist list is now sorted 💪

--- jammming/jammming/src/Components/UserPlaylists/UserPlaylists.js
+++ jammming/jammming/src/Components/UserPlaylists/UserPlaylists.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import UserPlaylist from '../UserPlaylist/UserPlaylist';
 
 class UserPlaylists extends Component {
+    
+    sortList = (property) => {
+        var sortOrder = 1;
+
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+
+    return function (a,b) {
+        if(sortOrder == -1){
+            return b[property].localeCompare(a[property]);
+        }else{
+            return a[property].localeCompare(b[property]);
+        }        
+    }
+    }
+    
     render(){
+        
+        const sortedList = this.props.playlists.sort(this.sortList('name'));
+        console.log(sortedList);
         return (
             <div>
             {
-             this.props.playlists.map(playlist => {
-                return <UserPlaylist onGetTracks={this.props.onGetTracks} key={playlist.id} playlist={playlist} />
+             sortedList.map(playlist => {
+              return <UserPlaylist onGetTracks={this.props.onGetTracks} key={playlist.id} playlist={playlist} />
             })
             }
             </div>
